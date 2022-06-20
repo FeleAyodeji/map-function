bbedc68fc4a4d6fb97f1463cd35cab347c0635c3
Fele Ayodeji <feledeji@gmail.com>
2022-06-20 04:07:04 -0700

    using arrow function

--- map-array.js
+++ map-array.js
@@ -5,23 +5,38 @@
 
 const numArr = [3,4,5,6,7,4,9]
 
-function double (x){
-    return x*2
-}
+// function double (x){
+//     return x*2
+// }
+// const output1 = numArr.map(double)
+// console.log(output1)  // printing out double of the array
 
-const output1 = numArr.map(double)
-console.log(output1)  // printing out double of the array
+//or
+const output1 = numArr.map((x)=> x*2)
+console.log(output1)
 
-function triple (x){
-    return x*3
-}
 
-const output2 = numArr.map(triple)
-console.log(output2)  //printing out triple of the array
 
-function binary (x){
-    return x.toString(2)
-}
+// function triple (x){
+//     return x*3
+// }
+// const output2 = numArr.map(triple)
+// console.log(output2)  //printing out triple of the array
 
-const output3 = numArr.map(binary)
-console.log(output3)  //print out the binary of the array
+//or
+
+const output2 = numArr.map((x)=> x*3)
+console.log(output2)
+
+
+// function binary (x){
+//     return x.toString(2)
+// }
+
+// const output3 = numArr.map(binary)
+// console.log(output3)  //print out the binary of the array
+
+//or
+
+const output3 = numArr.map((x)=> x.toString(2))
+console.log(output3)
